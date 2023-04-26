562de59025c2cf8aa854b5033ce92635c180b55b
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-04-26 17:30:13 -0300

    Tradução das mensagens ao se logar e deslogar da api.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   private
     def respond_with(resource, _opts = {})
-      render json: { message: 'Logged.' }, status: :ok
+      render json: { message: 'Logado com sucesso.' }, status: :ok
     end
 
     def respond_to_on_destroy
@@ -13,10 +13,10 @@ class Users::SessionsController < Devise::SessionsController
     end
 
     def log_out_success
-      render json: { message: "Logged out." }, status: :ok
+      render json: { message: "Desconectado com sucesso." }, status: :ok
     end
 
     def log_out_failure
-      render json: { message: "Logged out failure."}, status: :unauthorized
+      render json: { message: "Falha ao se desconectar."}, status: :unauthorized
     end
 end
